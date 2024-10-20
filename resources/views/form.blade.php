c23d228e353f9de271b590f6c885409c35664187
Kulikov Alexander <shouter84@yandex.ru>
2024-10-20 21:40:35 +0300

    migration executed

---
+++ resources/views/form.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Form</title>
+</head>
+<body>
+    <div class="add-books__form-wrapper">
+        <form action="{{route('store-form')}}" method="post" name="add-new-book" id="add-new-book">
+            @csrf
+            <div class="form-section">
+                <label for="title">Title</label>
+                <input type="text" id="title" name="title" class="form-control" required>
+            </div>
+            <div class="form-section">
+                <label for="author">Author</label>
+                <input type="text" id="author" name="author" class="form-control" required>
+            </div>
+            <div class="form-section">
+                <label for="genre">Choose Genre:</label>
+                <select name="genre" id="genre">
+                    <option value="fantasy">Fantasy</option>
+                    <option value="sci-fi">Sci-Fi</option>
+                    <option value="mystery">Mystery</option>
+                    <option value="drama">Drama</option>
+                </select>  
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</body>
+</html>
